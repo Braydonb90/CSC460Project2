980c3ae8c9a71785b95dcaf68f1fb38bc3cd294b
tristan <tristanp2@hotmail.com>
2018-04-14 22:40:25 -0700

    forgot to commit

--- project3/remote/remote_main.c
+++ project3/remote/remote_main.c
@@ -4,36 +4,24 @@
 #include "analog_io.h"
 #include <util/delay.h>
 
-void query_joystick() {
-    int since_last_print = 0;
-    int since_last_send = 0;
-    int i;
-    int buff_size = 20;
-    char buffer[20];
+void communication_init() {
+    uart1_init(BAUD_CALC(9600));
+}
+void send_data() PERIODIC_TASK(
+{
+    send_packet();
+}
+)
 
+void query_joystick() {
     //shouldnt this macro be called in uart1_init?
     uart1_init(BAUD_CALC(9600));
     for(;;) {
-        if(since_last_print > 500) {
-            int x0 = query_joystick_x(0);
-            int y0 = query_joystick_y(0);
-            int z0 = query_joystick_z(0);
-            int x1 = query_joystick_x(1);
-            int y1 = query_joystick_y(1);
-            int z1 = query_joystick_z(1);
-            int ls = get_laserstate();
-            since_last_print = 0;
-        }
-        else {
-            since_last_print += 20;
-        }
-        if(since_last_send > 250) {
-            send_packet();
-            since_last_send = 0;
-        }
-        else {
-            since_last_send += 20;
-        }
+        query_joystick_x(0);
+        query_joystick_y(0);
+        query_joystick_z(0);
+        query_joystick_x(1);
+        query_joystick_y(1);
         _delay_ms(20);
     }
 }
@@ -42,5 +30,7 @@ void user_main() {
     Task_Create_System(analog_init, 0);     
     Task_Create_System(servo_init, 0);
     Task_Create_System(joystick_init, 0);
+    Task_Create_System(communication_init, 0);
+    Task_Create_Periodic(send_packet, 0, 10, 5, 0);
     Task_Create_RR(query_joystick, 0);
 }
